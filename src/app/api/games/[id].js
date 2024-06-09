28cfc75bc6e6fd61bb340524947811a9e337a1f6
franciscojrc <cyberazul@gmail.com>
2024-06-09 22:56:38 +0200

    Cambios en endpoints y base de datos

--- src/app/api/games/[id].js
+++ src/app/api/games/[id].js
@@ -8,24 +8,45 @@ export default async function handler(req, res) {
   const { id } = req.query;
 
   if (req.method === "PUT") {
-    // Extrae el nuevo nombre del cuerpo de la solicitud (body)
-    const { nameGame } = req.body;
+    // Extrae el nuevo nombre y codeGame del cuerpo de la solicitud (body)
+    const { nameGame, detailGame, codeGame } = req.body;
 
-    // Valida que el nombre esté presente
-    if (!nameGame) {
-      return res
-        .status(400)
-        .json({ error: "El nombre del juego es requerido" });
+    // Valida que el nombre y el codeGame estén presentes
+    if (!nameGame || codeGame === undefined) {
+      return res.status(400).json({
+        error: "El nombre del juego y el código del juego son requeridos",
+      });
+    }
+
+    // Verifica que el codeGame sea un número entero de cuatro cifras
+    if (typeof codeGame !== "number" || codeGame < 1000 || codeGame > 9999) {
+      return res.status(400).json({
+        error:
+          "El código del juego debe ser un número entero de cuatro cifras (1000-9999)",
+      });
     }
 
     try {
-      // Actualiza el nombre del juego en la base de datos
-      const updateGame = await prisma.game.update({
+      // Comprueba si el codeGame ya existe en otro registro en la base de datos
+      const existingGame = await prisma.games.findUnique({
+        where: { codeGame },
+      });
+
+      if (existingGame && existingGame.id !== parseInt(id)) {
+        return res.status(409).json({
+          error: "El código del juego ya está en uso. Por favor, elija otro.",
+        });
+      }
+
+      // Actualiza el nombre del juego y el codeGame en la base de datos
+      const updateGame = await prisma.games.update({
         where: {
           id: parseInt(id), // Convierte el id a un número entero
         },
         data: {
           nameGame,
+          detailGame,
+          codeGame,
         },
       });
       // Devuelve el juego actualizado en formato JSON con un estado 200 (OK)
@@ -36,21 +57,68 @@ export default async function handler(req, res) {
     }
   } else if (req.method === "DELETE") {
     try {
-      // Elimina el juego de la base de datos
-      await prisma.game.delete({
+      // Obtén el juego a eliminar
+      const gameToDelete = await prisma.games.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      });
+
+      if (!gameToDelete) {
+        return res.status(404).json({ error: "Juego no encontrado" });
+      }
+
+      const { nameGame } = gameToDelete;
+
+      // Elimina primero todas las preguntas relacionadas en la tabla Asks
+      await prisma.asks.deleteMany({
+        where: {
+          gameId: parseInt(id), // Convierte el id a un número entero
+        },
+      });
+
+      // Luego elimina el juego de la tabla Games
+      await prisma.games.delete({
         where: {
           id: parseInt(id), // Convierte el id a un número entero
         },
       });
-      // Devuelve una respuesta vacía con un estado 204 (No Content)
-      res.status(204).end();
+
+      // Devuelve un mensaje indicando que el juego fue eliminado
+      res.status(200).json({ message: `El juego '${nameGame}' fue eliminado` });
     } catch (error) {
       // En caso de error, devuelve un estado 500 (Error del servidor) con un mensaje de error
-      res.status(500).json({ error: "Error al eliminar el juego" });
+      res
+        .status(500)
+        .json({
+          error: "Error al eliminar el juego y sus preguntas relacionadas",
+        });
+    }
+  } else if (req.method === "GET") {
+    try {
+      // Busca los juegos creados por el nickUser
+      const games = await prisma.games.findMany({
+        where: {
+          nickUser: id,
+        },
+      });
+
+      // Si no se encuentran juegos, devuelve un mensaje indicando esto
+      if (games.length === 0) {
+        return res.status(404).json({
+          message: "Este usuario no creó todavía ningún juego",
+        });
+      }
+
+      // Devuelve la lista de juegos en formato JSON con un estado 200 (OK)
+      res.status(200).json(games);
+    } catch (error) {
+      // En caso de error, devuelve un estado 500 (Error del servidor) con un mensaje de error
+      res.status(500).json({ error: "Error al buscar los juegos" });
     }
   } else {
-    // Si el método HTTP no es PUT o DELETE, devuelve un estado 405 (Método no permitido)
-    res.setHeader("Allow", ["PUT", "DELETE"]);
+    // Si el método HTTP no es PUT, DELETE o GET, devuelve un estado 405 (Método no permitido)
+    res.setHeader("Allow", ["PUT", "DELETE", "GET"]);
     res.status(405).end(`Método ${req.method} no permitido`);
   }
 }
--- src/app/api/games/index.js
+++ src/app/api/games/index.js
@@ -6,24 +6,47 @@ const prisma = new PrismaClient();
 export default async function handler(req, res) {
   // Verifica si el método HTTP de la solicitud es POST
   if (req.method === "POST") {
-    // Extrae el nombre y el nick del cuerpo de la solicitud (body)
-    const { nickUser, nameGame } = req.body;
+    // Extrae el nombre, el nick y el codeGame del cuerpo de la solicitud (body)
+    const { nickUser, nameGame, codeGame, detailGame } = req.body;
 
-    // Valida que el nombre y el nick estén presentes
-    if (!nickUser || !nameGame) {
-      return res
-        .status(400)
-        .json({ error: "Nombre del juego y nick del usuario son requeridos" });
+    // Valida que el nombre, el nick y el codeGame estén presentes
+    if (!nickUser || !nameGame || codeGame === undefined) {
+      return res.status(400).json({
+        error:
+          "Nombre del juego, nick del usuario y código del juego son requeridos",
+      });
+    }
+
+    // Verifica que el codeGame sea un número entero de cuatro cifras
+    if (typeof codeGame !== "number" || codeGame < 1000 || codeGame > 9999) {
+      return res.status(400).json({
+        error:
+          "El código del juego debe ser un número entero de cuatro cifras (1000-9999)",
+      });
     }
 
     try {
+      // Comprueba si el codeGame ya existe en la base de datos
+      const existingGame = await prisma.game.findUnique({
+        where: { codeGame },
+      });
+
+      if (existingGame) {
+        return res.status(409).json({
+          error: "El código del juego ya está en uso. Por favor, elija otro.",
+        });
+      }
+
       // Crea un nuevo juego en la base de datos
       const newGame = await prisma.game.create({
         data: {
           nickUser,
           nameGame,
+          detailGame,
+          codeGame,
         },
       });
+
       // Devuelve el ID del nuevo juego con un estado 201 (Creado)
       res.status(201).json({ id: newGame.id });
     } catch (error) {
