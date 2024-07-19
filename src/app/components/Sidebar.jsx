b778567346da295c11f41bb0e014321fcb1c4d58
Cris <criistiiniilla.1994@gmail.com>
2024-07-19 14:17:45 +0200

    nuevo avatar

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,27 +1,47 @@
-// components/Sidebar.js
 'use client';
 
-import Link from 'next/link';
 import { SignInButton, useUser } from '@clerk/nextjs';
 import { useEffect, useState } from 'react';
-import Avvvatars from 'avvvatars-react'
 import User from './User';
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [nickname, setNickname] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [mostrarAvatar, setMostrarAvatar] = useState(false);
   const { user } = useUser();
+  const apikey = process.env.apikey
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
-  useEffect(() => {
+  const avatars = async () => {
+    try {
+      const response = await fetch(`https://api.multiavatar.com/${nickname}.svg?apikey=${apikey}`
+        + JSON.stringify(nickname));
+      const svg = await response.text();
+      const adjustedSvg = svg.replace('<svg ', '<svg width="70" height="70" ');
+      setAvatar(adjustedSvg);
+    } catch (error) {
+      console.error('Error fetching avatar:', error);
+    }
+  };
+  avatars();
 
+  useEffect(() => {
     if (typeof window !== 'undefined') {
       const storedNickname = localStorage.getItem('nickname');
       if (storedNickname) {
         setNickname(storedNickname);
+        setMostrarAvatar(!mostrarAvatar)
+      }
+      if (user) {
+        localStorage.removeItem('nickname')
+      }
+
+      if (user && !nickname) {
+        setMostrarAvatar(mostrarAvatar)
       }
     }
   }, [nickname]);
@@ -30,7 +50,7 @@ const Sidebar = () => {
     <div className='sidebar'>
       {!isOpen ? (
         <button
-          className='p-4 text-white fixed top-4 right-4 z-50 '
+          className='p-4 text-white fixed top-4 right-4 z-50'
           onClick={toggleSidebar}
         >
           ☰
@@ -39,48 +59,31 @@ const Sidebar = () => {
         ''
       )}
       <div
-        className={`fixed top-0 right-0 h-full w-64 bg-[#533b7cf0]  shadow-xl  text-white transition-transform transform ${
+        className={`fixed top-0 right-0 h-full w-64 bg-black shadow-xl text-white transition-transform transform z-50 ${ // Aumentar z-index aquí
           isOpen ? 'translate-x-0' : 'translate-x-full'
-        }`}
+          }`}
       >
         <button className='p-4 text-white' onClick={toggleSidebar}>
           ✕
         </button>
-        <nav className='flex flex-col text-center gap-5 mt-5'>
+        <nav className='flex flex-col text-center gap-2 mt-5'>
           {user ? (
-            <>
+            <div className='ml-8'>
               <User />
-              <Link onClick={toggleSidebar} href={'/pages/access-pin'}>
-                Acess Pin
-              </Link>
-              <Link onClick={toggleSidebar} href={'/pages/control-quiz'}>
-                Control Quiz
-              </Link>
-              <Link onClick={toggleSidebar} href={'/pages/modify-quiz'}>
-                Modify Quiz
-              </Link>
-              <Link onClick={toggleSidebar} href={'/pages/demo-game'}>
-                Demo game
-              </Link>
-              <Link onClick={toggleSidebar} href={'/pages/nick-name-form'}>
-                NickNameForm
-              </Link>
-              <Link onClick={toggleSidebar} href={'/pages/ranking'}>
-                Ranking
-              </Link>
-              <Link onClick={toggleSidebar} href={'/pages/create-quiz'}>
-                Create Quiz
-              </Link>
-              <Link onClick={toggleSidebar} href={'/pages/start-quiz'}>
-                Start Quiz
-              </Link>
-            </>
+            </div>
           ) : (
-              <div className='flex flex-row gap-10'>
-                <Avvvatars value={nickname} style="shape" borderSize={2} size={50} radius={40} shadow={true} />
-                <p className='flex flex-row gap-2 justify-center items-center'>{nickname}</p>
-                <SignInButton className='mr-10' />
-              </div>
+            <div className='flex flex-col justify-center items-center text-center w-full h-full gap-2'>
+              {
+                !mostrarAvatar && nickname &&
+                <div className='border-2 border-white rounded-full' dangerouslySetInnerHTML={{ __html: avatar }} />
+              }
+              {
+                mostrarAvatar && !nickname &&
+                <div className='border-2 border-white rounded-full' dangerouslySetInnerHTML={{ __html: avatar }} />
+              }
+              <p className='flex flex-row gap-2 justify-center items-center'>{nickname}</p>
+              <SignInButton />
+            </div>
           )}
         </nav>
       </div>
