b71d8f828260bb71987fa332ccdcbcb584e7de55
Cris <criistiiniilla.1994@gmail.com>
2024-07-19 09:39:03 +0200

    agregado animacion en page ranking

--- src/app/pages/ranking/page.jsx
+++ src/app/pages/ranking/page.jsx
@@ -1,13 +1,32 @@
+'use client';
 
+import React, { useRef } from 'react';
+import Confetti from '../../../lib/utils';
 
-function rankingPage() {
- 
+function RankingPage() {
+  const confettiRef = useRef(null);
+
   return (
-    <div>
-      rankingPage
-      <button>Bye</button>
+    <div className="relative flex flex-col items-center justify-center h-[400px] w-full rounded-lg  text-slate-500 uppercase sm:h-[500px] md:h-[600px] lg:h-[700px]">
+      <p className="mb-4 text-center text-lg sm:text-xl md:text-2xl">Haz click en el texto</p>
+      <span className="pointer-events-none whitespace-pre-wrap  bg-clip-text text-center text-4xl font-semibold leading-none text-white sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl dark:from-white dark:to-slate-900/10 select-none">
+        ¡Juego Completado!
+      </span>
+
+      <Confetti
+        ref={confettiRef}
+        className="absolute left-0 top-0 z-0 w-full h-full"
+        onClick={() => {
+          confettiRef.current?.fire();
+        }}
+        options={{
+          get angle() {
+            return Math.random() * 360;
+          },
+        }}
+      />
     </div>
-  )
+  );
 }
 
-export default rankingPage
+export default RankingPage;
