553d39d0700dfd82aac33f5f7152ad5e368b5c2a
Cris <criistiiniilla.1994@gmail.com>
2024-07-19 09:29:52 +0200

    agregado removeItem nickname

--- src/app/components/User.jsx
+++ src/app/components/User.jsx
@@ -2,14 +2,11 @@
 
 import { useEffect, useState } from 'react';
 import { SignedIn, UserButton, useUser } from '@clerk/nextjs'
-import Avvvatars from 'avvvatars-react'
-
 
 
 export default function User() {
   const [nickname, setNickname] = useState('');
   const { user } = useUser();
-
 
   useEffect(() => {
 
@@ -19,29 +16,34 @@ export default function User() {
         setNickname(storedNickname);
       }
 
+      if (user) {
+        localStorage.removeItem('nickname')
+      }
+
     }
-  }, [nickname]);
+  }, [user, nickname]);
 
   return (
     <>
-      <div className='flex justify-center gap-10 w-full'>
+      <div className='flex justify-center gap-2 w-full mb-8'>
         <SignedIn  >
-          <div className="ml-10 mt-5">
+          <div className="">
             <UserButton appearance={{
               elements: {
-                userButtonAvatarBox: 'w-16 h-16 rounded-full border-2  border-gray-300'
+                userButtonAvatarBox: 'w-16 h-16 rounded-full border-2  border-slate-300'
               }
             }} />
           </div>
         </SignedIn>
         {
-          user && !nickname && user.firstName ? <p className='mt-12 mr-12'>{user.firstName} {user.lastName}</p> : ''
+          user ? <p className='mt-4 mr-12 font-bold bg-black h-8 px-2  rounded-md'>{user.firstName} {user.lastName}</p> : ''
         }
         {
-          nickname && !user ? <p className='flex flex-row gap-2 justify-center items-center'><Avvvatars value={nickname} style="shape" borderSize={2} size={60} radius={40} shadow={true} />{nickname}</p> : ''
-        }
-        {
-          !user?.firstName ? <p className='mt-12 mr-12'>{user?.username}</p> : ''
+          nickname && !user ?
+
+            <p className='flex flex-row gap-2 justify-center items-center bg-black h-8 px-2 '>{nickname}</p>
+
+            : ''
         }
 
       </div>
