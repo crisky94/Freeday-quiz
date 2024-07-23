ad729207c2230541161df8461422b6aed26a3534
Cris <criistiiniilla.1994@gmail.com>
2024-07-23 19:45:12 +0200

    creado fetchAvatar

---
+++ src/lib/fetchAvatar.js
@@ -0,0 +1,24 @@
+'use client'
+import { useState } from 'react';
+
+const apikey = process.env.REACT_APP_API_KEY; // Asegúrate de usar el prefijo REACT_APP_ para las variables de entorno en create-react-app
+
+const useAvatar = () => {
+  const [avatar, setAvatar] = useState();
+
+  const avatars = async (nickname) => {
+    try {
+      const response = await fetch(`https://api.multiavatar.com/${nickname}.svg?apikey=${apikey}`);
+      const svg = await response.text();
+      const adjustedSvg = svg.replace('<svg ', '<svg width="50" height="50" ');
+      setAvatar(adjustedSvg);
+    } catch (error) {
+      console.error('Error fetching avatar:', error);
+    }
+    return avatar;
+  };
+
+  return { avatar, avatars };
+};
+
+export default useAvatar;
