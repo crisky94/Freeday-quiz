fa76285455ec32a0c12424c66f44a34bddc039c7
Cris <criistiiniilla.1994@gmail.com>
2024-08-06 15:38:30 +0200

    ranking y envio de ranking en c-q

--- src/app/pages/ranking/[code]/page.jsx
+++ src/app/pages/ranking/[code]/page.jsx
@@ -1,67 +1,13 @@
-// 'use client';
-
-// import React, { useEffect, useRef, useState } from 'react';
-// import Confetti from '../../../../lib/utils';
-// import RankingModal from '../../../components/RankingModal';
-// import { useSocket } from '@/context/socketContext';
-
-// function RankingPage() {
-//   const confettiRef = useRef(null);
-//   const [ranking, setRanking] = useState([]);
-//   const socket = useSocket();
-//   const [showRankingModal, setShowRankingModal] = useState(false);
-//   // const code =123456;
-  
-//   useEffect(() => {
-//     if (!socket) return;
-
-//     const handleRedirectToFinalScreen = (data) => {
-//       console.log(data);
-//       setRanking(data.ranking); 
-//       setShowRankingModal(true);
-//     };
-
-//     socket.on('redirectToFinalScreen', handleRedirectToFinalScreen);
-//     return () => {
-//       socket.off('redirectToFinalScreen', handleRedirectToFinalScreen);
-//     };
-//   }, [socket]);
-   
-//   return (
-//     <>
-//     <div className="relative flex flex-col items-center justify-center h-[400px] w-full rounded-lg  text-slate-600 uppercase sm:h-[500px] md:h-[600px] lg:h-[700px] min-h-screen bg-white bg-opacity-80">
-//       <p className="mb-4 text-center text-lg sm:text-xl md:text-2xl">Haz click en el texto</p>
-//       <span className="pointer-events-none whitespace-pre-wrap  bg-clip-text text-center text-4xl font-semibold leading-none text-[#111] sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl dark:from-white dark:to-slate-900/10 select-none ">
-//         ¡Juego Completado!
-//       </span>
-
-//       <Confetti
-//         ref={confettiRef}
-//         className="absolute left-0 top-0 z-0 w-full h-full"
-//         onClick={() => {
-//           confettiRef.current?.fire();
-//         }}
-//         options={{
-//           get angle() {
-//             return Math.random() * 360;
-//           },
-//         }}
-//       />
-//     </div>
-//       <RankingModal ranking={ranking}/>     
-//     </>
-//   );
-// }
-
-// export default RankingPage;
 'use client';
 
 import React, { useEffect, useRef, useState } from 'react';
+// import { useRouter } from 'next/navigation';
 import Confetti from '../../../../lib/utils';
 import { useSocket } from '@/context/socketContext';
 
 function RankingPage() {
   const confettiRef = useRef(null);
+  // const router = useRouter()
   const [ranking, setRanking] = useState([]);
   const socket = useSocket();
   const [socketId, setSocketId] = useState();
@@ -90,6 +36,17 @@ function RankingPage() {
     };
   }, [socket]);
 
+
+  // useEffect(() => {
+  //   socket.on('deletePlayer', (playerId) => {
+  //     setPlayers((prevPlayers) => prevPlayers.filter(player => player.id !== playerId));
+  //   });
+
+  //   return () => {
+  //     socket.off('deletePlayer');
+  //   };
+  // }, [socket]);
+
   if (isLoading) {
  
     return (
@@ -99,7 +56,6 @@ function RankingPage() {
         <span className="pointer-events-none whitespace-pre-wrap  bg-clip-text text-center text-4xl font-semibold leading-none text-[#111] sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl dark:from-white dark:to-slate-900/10 select-none ">
           ¡Juego Completado!
         </span>
-
         <Confetti
           ref={confettiRef}
           className="absolute left-0 top-0 z-0 w-full h-full"
@@ -113,31 +69,34 @@ function RankingPage() {
           }}
         />
       </div>
-    </>
- 
+    </> 
 );
 }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen w-full pt-16">
-      <div className='bg-custom-linear flex mb-2'>
-        <div className='flex flex-col p-14 m-1 h-auto w-58 sm:w-full items-center bg-black gap-5'>
-          <h1 className='uppercase font-bold text-xl text-center'>Ranking Final</h1>
-          <ul className='w-full text-left text-black border'>
-            {ranking
-              .sort((a, b) => b.score - a.score)  // Ordenar de mayor a menor
-              .map((player, index) => (
-                <li key={index} className={`w-14 flex flex-col items-center p-1 mx-8 ${player.socketId === socketId ? 'text-secundary' : 'text-white'
-                  }`}>
-                  {player.playerName}: {player.score}
-                </li>
-              ))}
-          </ul>
-      <div className='text-xl font-bold text-center mt-4 text-yellow-500'>
-         Gracias por jugar.
-      </div>
-        </div>
-      </div>
-    </div>)
+    <div className='flex flex-col p-2 h-auto items-center bg-black text-white w-full pt-20 min-h-screen'>
+      <h1 className='uppercase font-bold text-xl md:text-2xl text-center mb-3'>Ranking</h1>
+      <table className='w-full text-left'>
+        <tbody className='w-full text-white flex flex-col justify-center items-center'>
+          {ranking
+            .slice(0, 8)
+            .map((player, index) => (
+              <tr
+                key={index}
+                className={`w-full max-w-xs md:max-w-md flex items-center justify-between p-2 ${player.socketId === socketId ? 'bg-yellow-200' : 'bg-white'} bg-opacity-40 rounded-md mb-1`}
+              >
+                <td className='flex items-center'>
+                  {index === 0 && <p className="fas fa-trophy text-yellow-500 mr-2 text-lg">1</p>}
+                  {index === 1 && <p className="fas fa-medal text-gray-400 mr-2 text-lg">2</p>}
+                  {index === 2 && <p className="fas fa-medal text-amber-600 mr-2 text-lg">3</p>}
+                  <span className='font-semibold text-xs md:text-sm'>{player.playerName}</span>
+                </td>
+                <td className='text-right font-bold text-yellow-500 text-xs md:text-sm'>{player.score}px</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+)
 }
 
 export default RankingPage;
