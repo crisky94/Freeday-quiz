29b661a2ec3aadc21c49f8953f760798e333a3e6
Cris <criistiiniilla.1994@gmail.com>
2024-08-09 09:46:24 +0200

    agregado avatar en ranking

--- src/app/pages/ranking/[code]/page.jsx
+++ src/app/pages/ranking/[code]/page.jsx
@@ -4,7 +4,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Confetti from '../../../../lib/utils';
 import { useSocket } from '@/context/socketContext';
+import { useAvatar } from '@/context/avatarContext';
 import { ToastContainer, toast } from 'react-toastify';
+import Image from 'next/image';
 import 'react-toastify/dist/ReactToastify.css';
 
 function RankingPage() {
@@ -14,17 +16,30 @@ function RankingPage() {
   const socket = useSocket();
   const [socketId, setSocketId] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [avatars, setAvatars] = useState({});
+  const { fetchAvatar } = useAvatar();
 
   useEffect(() => {
     if (!socket) return;
     setSocketId(socket.id);
     // Listener para recibir el ranking final
-    const handleFinalRanking = (response) => {
+    const handleFinalRanking = async (response) => {
       if (response.error) {
         console.error(response.error);
       } else {
         setRanking(response.ranking);
         setIsLoading(false);
+        const avatarsData = await Promise.all(
+          response.ranking.map(async (player) => {
+            const avatar = await fetchAvatar(player.playerName);
+            return { id: player.id, avatar };
+          })
+        );
+        const avatarsMap = {};
+        avatarsData.forEach(({ id, avatar }) => {
+          avatarsMap[id] = avatar;
+        });
+        setAvatars(avatarsMap);
       }
     };
 
@@ -87,9 +102,18 @@ function RankingPage() {
                   {index === 0 && <p className="fas fa-trophy text-yellow-500 mr-2 text-lg">1</p>}
                   {index === 1 && <p className="fas fa-medal text-gray-400 mr-2 text-lg">2</p>}
                   {index === 2 && <p className="fas fa-medal text-amber-600 mr-2 text-lg">3</p>}
+                  <Image
+                  className='mr-6 ml-2'
+                    width={40}
+                    height={40}
+                    src={`data:image/svg+xml;utf8,${encodeURIComponent(
+                      avatars[player.id]
+                    )}`}
+                    alt={`${player.playerName}'s avatar`}
+                  />
                   <span className='font-semibold text-xs md:text-sm'>{player.playerName}</span>
                 </td>
-                <td className='text-right font-bold text-yellow-500 text-xs md:text-sm'>{player.score}px</td>
+                <td className='text-right font-bold text-yellow-500 text-xs md:text-sm mr-2'>{player.score}px</td>
               </tr>
             ))}
         </tbody>
