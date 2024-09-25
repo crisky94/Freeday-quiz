efbda2436cc60eef696c7975f769f513f2f76b6d
Cris <criistiiniilla.1994@gmail.com>
2024-09-25 17:01:59 +0200

    numero de preguntas en games

--- src/app/pages/create-quiz/page.jsx
+++ src/app/pages/create-quiz/page.jsx
@@ -16,7 +16,8 @@ export default function CreateGame() {
   const router = useRouter();
   const socket = useSocket();
   const { user } = useAuth(User);
-
+  const [previewImage, setPreviewImage] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
   const [nameGame, setNameGame] = useState('');
   const [nickUser, setNickUser] = useState('');
   const [asks, setAsks] = useState([]);
@@ -31,6 +32,7 @@ export default function CreateGame() {
   const [pin, setPin] = useState('');
   const [detailGame, setDetailGame] = useState('');
   const [gameId, setGameId] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
 
   const isValidInput = (input) => {
     return input.trim() !== ''; // trim quita los espacios en blanco al inicio y al final y luego verifica que el input no sea una cadena vacía
@@ -99,7 +101,7 @@ export default function CreateGame() {
     }
   };
 
-  const addOrUpdateAsk = () => {
+  const addOrUpdateAsk = async () => {
     if (
       !nameGame.trim() ||
       !nickUser.trim() ||
@@ -141,6 +143,27 @@ export default function CreateGame() {
       return;
     }
 
+    // Upload image if there is one
+    let imageUrl = null;
+    if (selectedFile) {
+      try {
+        imageUrl = await uploadImage(selectedFile);
+      } catch (error) {
+        toast.error('Error al subir la imagen.', {
+          position: 'bottom-center',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+          transition: Flip,
+        });
+        return;
+      }
+    }
+
     const newAsk = {
       ask: currentAsk,
       a: answers[0],
@@ -152,29 +175,34 @@ export default function CreateGame() {
       isCorrectC,
       isCorrectD,
       timer: numericTimeLimit,
+      image: imageUrl || null,
     };
 
     let updatedAsks;
     if (editIndex !== null) {
       updatedAsks = [...asks];
       updatedAsks[editIndex] = newAsk;
-      setAsks(updatedAsks);
+      setImageUrls(
+        imageUrls.map((url, i) => (i === editIndex ? imageUrl : url))
+      );
       setEditIndex(null);
     } else {
       updatedAsks = [...asks, newAsk];
-      setAsks(updatedAsks);
+      setImageUrls([...imageUrls, imageUrl]);
     }
+    setAsks(updatedAsks);
     localStorage.setItem('asks', JSON.stringify(updatedAsks));
 
+    // Reset form state
     setCurrentAsk('');
     setAnswers(['', '', '', '']);
-    setIsCorrectA(false); // Restablecer estados
+    setIsCorrectA(false);
     setIsCorrectB(false);
     setIsCorrectC(false);
     setIsCorrectD(false);
     setTimer('5');
-    localStorage.removeItem('answers');
-    localStorage.removeItem('correctAnswer');
+    setPreviewImage(null);
+    setSelectedFile(null);
   };
 
   const handleEdit = (index) => {
@@ -199,57 +227,53 @@ export default function CreateGame() {
     setAsks(updatedAsks);
     localStorage.setItem('asks', JSON.stringify(updatedAsks));
   };
+  // Función que maneja la selección de imagen y actualiza la vista previa
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      console.log('Selected file:', file); // Verifica el archivo seleccionado
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (editIndex !== null) {
-      addOrUpdateAsk();
-      return;
+      const imageUrl = URL.createObjectURL(file);
+
+      setPreviewImage(imageUrl); // Guarda el archivo en lugar del Data URL
+      setSelectedFile(file); // Guarda el archivo para subirlo más tarde
     }
+  };
 
-    if (
-      !isValidInput(nameGame) ||
-      !isValidInput(nickUser) ||
-      asks.length === 0
-    ) {
-      toast.error('Completa el título y al menos una pregunta.', {
-        position: 'bottom-center',
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: 'light',
-        transition: Flip,
+  const handleDeleteImg = () => {
+    if (previewImage) {
+      // URL.revokeObjectURL(previewImage); // Libera la URL
+    }
+    setPreviewImage(null);
+  };
+
+  //* funcion de envio >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // Function to handle image upload
+  const uploadImage = async (file) => {
+    const formData = new FormData();
+    formData.append('image', file);
+
+    try {
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
       });
-      return;
+      const data = await response.json();
+      console.log('Upload response:', data); // Verifica la respuesta del servidor
+
+      if (data.error) {
+        throw new Error('Error al subir imagen.');
+      }
+      return data.imageUrl;
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      throw error;
     }
+  };
 
-    const gameData = {
-      nameGame: nameGame.trim(),
-      detailGame: detailGame,
-      nickUser: nickUser.trim(),
-      asks: asks.map((ask) => ({
-        ask: ask.ask,
-        a: ask.a,
-        b: ask.b,
-        c: ask.c,
-        d: ask.d,
-        isCorrectA: ask.isCorrectA,
-        isCorrectB: ask.isCorrectB,
-        isCorrectC: ask.isCorrectC,
-        isCorrectD: ask.isCorrectD,
-        timer: ask.timer,
-      })),
-    };
-
-    // Enviar juego a través del socket
+  const createQuiz = (gameData) => {
     socket.emit('createGame', gameData, (response) => {
-      console.log(response, 'patata');
-
-      // Suponiendo que el backend devuelve un objeto con `pin` y `gameId`
-      if (response.game.codeGame) {
+      if (response.game && response.game.codeGame) {
         setPin(response.game.codeGame);
         setGameId(response.game.id);
         localStorage.removeItem('asks');
@@ -281,15 +305,70 @@ export default function CreateGame() {
         });
       }
     });
-    setNameGame('');
-    setAnswers(['', '', '', '']);
-    setAsks([]);
-    setIsCorrectA(false); // Restablecer estados
-    setIsCorrectB(false);
-    setIsCorrectC(false);
-    setIsCorrectD(false);
-    setCurrentAsk('');
-    setDetailGame('');
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (editIndex !== null) {
+      addOrUpdateAsk();
+      return;
+    }
+
+    if (
+      !isValidInput(nameGame) ||
+      !isValidInput(nickUser) ||
+      asks.length === 0
+    ) {
+      toast.error('Completa el título y al menos una pregunta.', {
+        position: 'bottom-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+        transition: Flip,
+      });
+      return;
+    }
+
+    try {
+      // Prepare the game data to be sent to the server
+      const gameData = {
+        nameGame: nameGame.trim(),
+        detailGame: detailGame,
+        nickUser: nickUser.trim(),
+        asks: asks.map((ask, index) => ({
+          ...ask,
+          image: imageUrls[index] || null,
+        })),
+      };
+
+      console.log('Submitting game data:', gameData);
+      createQuiz(gameData);
+    } catch (error) {
+      toast.error('Error en la creación del juego.', {
+        position: 'bottom-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+        transition: Flip,
+      });
+    }
+
+    localStorage.removeItem('nameGame');
+    localStorage.removeItem('currentAsk');
+    localStorage.removeItem('timer');
+    localStorage.removeItem('answers');
+    localStorage.removeItem('correctAnswer');
+    localStorage.removeItem('asks');
+    localStorage.removeItem('detail');
   };
 
   return (
@@ -328,7 +407,6 @@ export default function CreateGame() {
             onChange={(e) => setCurrentAsk(e.target.value)}
           />
         </div>
-
         <div className='flex flex-col justify-center items-center h-12 gap-2'>
           <label className='flex justify-center items-center h-6 text-sm rounded-md bg-[#111] '>
             Temporizador (segundos)
@@ -351,6 +429,98 @@ export default function CreateGame() {
             />
           </Tooltip>
         </div>
+        <div className='w-full h-96 my-2 rounded-md  flex justify-center items-center'>
+          <div className='flex flex-col items-center rounded-md justify-center drop w-96 h-full'>
+            {previewImage ? (
+              <div className='h-80 relative w-80'>
+                <img
+                  src={previewImage}
+                  alt='Preview'
+                  className='w-full h-full object-fit  bg-center rounded-md'
+                />
+                <button
+                  className='button absolute bottom-11 left-5 '
+                  onClick={handleDeleteImg}
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 69 14'
+                    className='svgIcon bin-top'
+                  >
+                    <g clipPath='url(#clip0_35_24)'>
+                      <path
+                        fill='black'
+                        d='M20.8232 2.62734L19.9948 4.21304C19.8224 4.54309 19.4808 4.75 19.1085 4.75H4.92857C2.20246 4.75 0 6.87266 0 9.5C0 12.1273 2.20246 14.25 4.92857 14.25H64.0714C66.7975 14.25 69 12.1273 69 9.5C69 6.87266 66.7975 4.75 64.0714 4.75H49.8915C49.5192 4.75 49.1776 4.54309 49.0052 4.21305L48.1768 2.62734C47.3451 1.00938 45.6355 0 43.7719 0H25.2281C23.3645 0 21.6549 1.00938 20.8232 2.62734ZM64.0023 20.0648C64.0397 19.4882 63.5822 19 63.0044 19H5.99556C5.4178 19 4.96025 19.4882 4.99766 20.0648L8.19375 69.3203C8.44018 73.0758 11.6746 76 15.5712 76H53.4288C57.3254 76 60.5598 73.0758 60.8062 69.3203L64.0023 20.0648Z'
+                      ></path>
+                    </g>
+                    <defs>
+                      <clipPath id='clip0_35_24'>
+                        <rect fill='white' height='14' width='60'></rect>
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 69 57'
+                    className='svgIcon bin-bottom'
+                  >
+                    <g clipPath='url(#clip0_35_22)'>
+                      <path
+                        fill='black'
+                        d='M20.8232 -16.3727L19.9948 -14.787C19.8224 -14.4569 19.4808 -14.25 19.1085 -14.25H4.92857C2.20246 -14.25 0 -12.1273 0 -9.5C0 -6.8727 2.20246 -4.75 4.92857 -4.75H64.0714C66.7975 -4.75 69 -6.8727 69 -9.5C69 -12.1273 66.7975 -14.25 64.0714 -14.25H49.8915C49.5192 -14.25 49.1776 -14.4569 49.0052 -14.787L48.1768 -16.3727C47.3451 -17.9906 45.6355 -19 43.7719 -19H25.2281C23.3645 -19 21.6549 -17.9906 20.8232 -16.3727ZM64.0023 1.0648C64.0397 0.4882 63.5822 0 63.0044 0H5.99556C5.4178 0 4.96025 0.4882 4.99766 1.0648L8.19375 50.3203C8.44018 54.0758 11.6746 57 15.5712 57H53.4288C57.3254 57 60.5598 54.0758 60.8062 50.3203L64.0023 1.0648Z'
+                      ></path>
+                    </g>
+                    <defs>
+                      <clipPath id='clip0_35_22'>
+                        <rect fill='white' height='57' width='79'></rect>
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </button>
+              </div>
+            ) : (
+              // Si ya hay una imagen cargada, mostrarla como vista previa
+
+              // Si no hay imagen cargada, mostrar el mensaje y el botón
+              <>
+                <p className='text-white font-bold'>Selecciona una imagen</p>
+                <button
+                  title='Add New'
+                  type='button'
+                  className='group cursor-pointer outline-none hover:rotate-90 duration-300'
+                  onClick={() => document.getElementById('imageInput').click()} // Simular el click en el input oculto
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='50px'
+                    height='50px'
+                    viewBox='0 0 24 24'
+                    className='stroke-lime-400 fill-none group-hover:fill-lime-800 group-active:stroke-lime-200 group-active:fill-lime-600 group-active:duration-0 duration-300'
+                  >
+                    <path
+                      d='M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z'
+                      strokeWidth='1.5'
+                    ></path>
+                    <path d='M8 12H16' strokeWidth='1.5'></path>
+                    <path d='M12 16V8' strokeWidth='1.5'></path>
+                  </svg>
+                </button>
+
+                {/* Input de tipo file oculto */}
+                <input
+                  id='imageInput'
+                  type='file'
+                  accept='image/*'
+                  onChange={handleImageChange}
+                  className='hidden' // Escondemos el input
+                />
+              </>
+            )}
+          </div>
+        </div>
+        {/* <InputFile /> */}
 
         <div className='grid grid-cols-1 m-5 sm:grid-cols-2 gap-4'>
           {answers.map((answer, index) => (
@@ -363,12 +533,12 @@ export default function CreateGame() {
                 index === 0
                   ? isCorrectA
                   : index === 1
-                  ? isCorrectB
-                  : index === 2
-                  ? isCorrectC
-                  : index === 3
-                  ? isCorrectD
-                  : false
+                    ? isCorrectB
+                    : index === 2
+                      ? isCorrectC
+                      : index === 3
+                        ? isCorrectD
+                        : false
               }
               onSelect={() => handleSelectAnswer(index)}
             />
